0f22e22cac6d06efba21c73f10d18b787c50f3ed
Zachary-Ronayne <zronayne42@gmail.com>
2023-06-18 22:12:19 -0400

    Can disable and enable menu buttons

--- src/zgame/menu/MenuButton.java
+++ src/zgame/menu/MenuButton.java
@@ -21,6 +21,12 @@ public class MenuButton extends MenuText{
 	/** The amount of time, in milliseconds that can pass between clicks for it to count as a double click */
 	private long doubleClickThreshold;
 	
+	/** true if this button is currently disabled and clicking will do nothing, false otherwise */
+	private boolean disabled;
+	
+	/** A color that will be displayed on top of this button's bounds when it is disabled */
+	private ZColor disableOverlay;
+	
 	/**
 	 * Create a blank {@link MenuButton} at the given position and size
 	 *
@@ -50,11 +56,19 @@ public class MenuButton extends MenuText{
 		
 		this.lastClick = -1;
 		this.doubleClickThreshold = DEFAULT_DOUBLE_CLICK_THRESHOLD;
+		
+		this.enable();
+		this.setDisableOverlay(new ZColor(.3, .6));
 	}
 	
 	@Override
 	public void render(Game game, Renderer r, ZRect bounds){
 		super.render(game, r, bounds);
+		if(this.isDisabled()){
+			r.setColor(this.getDisableOverlay());
+			r.drawRectangle(bounds);
+			return;
+		}
 		if(this.showHighlight(game)){
 			r.setColor(this.getHighlightColor());
 			r.drawRectangle(bounds);
@@ -67,6 +81,8 @@ public class MenuButton extends MenuText{
 	@Override
 	public boolean mouseAction(Game game, int button, boolean press, boolean shift, boolean alt, boolean ctrl){
 		boolean input = super.mouseAction(game, button, press, shift, alt, ctrl);
+		if(this.isDisabled()) return input;
+		
 		ZMouseInput mi = game.getMouseInput();
 		if(!press && this.getBounds().contains(mi.x(), mi.y())){
 			if(System.currentTimeMillis() - this.getLastClick() <= this.getDoubleClickThreshold()) this.doubleClick(game);
@@ -129,4 +145,33 @@ public class MenuButton extends MenuText{
 		this.doubleClickThreshold = doubleClickThreshold;
 	}
 	
+	/** @return See {@link #disabled} */
+	public boolean isDisabled(){
+		return this.disabled;
+	}
+	
+	/** @param disabled See {@link #disabled} */
+	public void setDisabled(boolean disabled){
+		this.disabled = disabled;
+	}
+	
+	/** Set {@link #disabled} to true */
+	public void disable(){
+		this.setDisabled(true);
+	}
+	
+	/** Set {@link #disabled} to false */
+	public void enable(){
+		this.setDisabled(false);
+	}
+	
+	/** @return See {@link #disableOverlay} */
+	public ZColor getDisableOverlay(){
+		return this.disableOverlay;
+	}
+	
+	/** @param disableOverlay See {@link #disableOverlay} */
+	public void setDisableOverlay(ZColor disableOverlay){
+		this.disableOverlay = disableOverlay;
+	}
 }
