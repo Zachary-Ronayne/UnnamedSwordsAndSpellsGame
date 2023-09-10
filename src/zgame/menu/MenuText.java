59033ef732f65cebaf8a3262cd87a044ab348627
Zachary-Ronayne <zronayne42@gmail.com>
2023-09-10 19:22:39 -0400

    Drawing text buffers using text options

--- src/zgame/menu/MenuText.java
+++ src/zgame/menu/MenuText.java
@@ -2,12 +2,14 @@ package zgame.menu;
 
 import zgame.core.Game;
 import zgame.core.graphics.Renderer;
+import zgame.core.graphics.TextOption;
 import zgame.core.graphics.ZColor;
 import zgame.core.graphics.font.GameFont;
 import zgame.core.graphics.font.TextBuffer;
+import zgame.core.utils.ZArrayUtils;
 import zgame.core.utils.ZRect;
 
-import java.util.Objects;
+import java.util.ArrayList;
 
 /**
  * A {@link MenuThing} that holds text Note, for this class and anything that extends it, calls to updating the width and height will not update the text buffer's width and
@@ -18,12 +20,6 @@ public class MenuText extends MenuThing{
 	
 	/** The {@link TextBuffer} which this {@link MenuText} will use to draw text */
 	private final TextBuffer textBuffer;
-	
-	/** The {@link ZColor} to use to color {@link #text} */
-	private ZColor fontColor;
-	
-	/** The text to display for this menu */
-	private String text;
 	
 	/** When rendering text, force the bounds to be limited to the intersection of bounds of this thing and the current limited bounds, false otherwise */
 	private boolean limitIntersectionBounds;
@@ -51,17 +47,43 @@ public class MenuText extends MenuThing{
 	 * @param game The game associated with this thing
 	 */
 	public MenuText(double x, double y, double w, double h, String text, Game game){
+		this(x, y, w, h, new TextOption(text), game);
+	}
+	
+	/**
+	 * Create a {@link MenuText} at the given position and size
+	 *
+	 * @param x See {@link #getX()}
+	 * @param y See {@link #getY()}
+	 * @param w See {@link #getWidth()}
+	 * @param h See {@link #getHeight()}
+	 * @param option The single element for See #options
+	 * @param game The game associated with this thing
+	 */
+	public MenuText(double x, double y, double w, double h, TextOption option, Game game){
+		this(x, y, w, h, ZArrayUtils.singleList(option), game);
+	}
+	
+	/**
+	 * Create a {@link MenuText} at the given position and size
+	 *
+	 * @param x See {@link #getX()}
+	 * @param y See {@link #getY()}
+	 * @param w See {@link #getWidth()}
+	 * @param h See {@link #getHeight()}
+	 * @param options See {@link TextBuffer#options}
+	 * @param game The game associated with this thing
+	 */
+	public MenuText(double x, double y, double w, double h, ArrayList<TextOption> options, Game game){
 		super(x, y, w, h);
-		this.text = text;
+		
 		// Using zfont by default
-		this.textBuffer = new TextBuffer((int)Math.round(w), (int)Math.round(h), game.getFont("zfont"));
+		this.textBuffer = new TextBuffer((int)Math.round(w), (int)Math.round(h), options, game.getFont("zfont"));
 		this.setTextX(10);
 		this.setTextY(this.getHeight() * .9);
 		this.textBuffer.setForceUnlimit(false);
 		
 		this.setFill(this.getFill().solid());
-		
-		this.fontColor = new ZColor(0);
 		
 		this.setLimitIntersectionBounds(true);
 	}
@@ -85,15 +107,16 @@ public class MenuText extends MenuThing{
 	
 	/** @return true if this text box contains no text, false otherwise */
 	public boolean isEmpty(){
-		return this.text == null || this.text.isEmpty();
+		var t = this.getText();
+		return t == null || t.isEmpty();
 	}
 	
-	/** @return See {@link #text} */
+	/** @return The raw string value displayed by this thing, ignoring all other values in this thing's buffer's {@link TextBuffer#options} */
 	public String getText(){
-		return this.text;
+		return this.textBuffer.getText();
 	}
 	
-	/** @return The value of {@link #text} assuming it's an integer. If it cannot be parsed as an integer, returns null */
+	/** @return The value of {@link #getText()} assuming it's an integer. If it cannot be parsed as an integer, returns null */
 	public Integer getTextAsInt(){
 		try{
 			return Integer.parseInt(this.getText());
@@ -102,7 +125,7 @@ public class MenuText extends MenuThing{
 		}
 	}
 	
-	/** @return The value of {@link #text} assuming it's a double. If it cannot be parsed as a double, returns null */
+	/** @return The value of {@link #getText()} assuming it's a double. If it cannot be parsed as a double, returns null */
 	public Double getTextAsDouble(){
 		try{
 			return Double.parseDouble(this.getText());
@@ -111,11 +134,9 @@ public class MenuText extends MenuThing{
 		}
 	}
 	
-	/** @param text See {@link #text} */
+	/** @param text Set the full text used by this thing, this will override anything in this thing's buffer's {@link TextBuffer#options} */
 	public void setText(String text){
-		if(Objects.equals(text, this.text)) return;
-		this.text = text;
-		this.getTextBuffer().updateRedraw(true);
+		this.textBuffer.setText(text);
 	}
 	
 	/** @return See {@link TextBuffer#textX} */
@@ -153,14 +174,12 @@ public class MenuText extends MenuThing{
 		return this.textBuffer;
 	}
 	
-	/** @return See {@link #fontColor} */
-	public ZColor getFontColor(){
-		return this.fontColor;
-	}
-	
-	/** @param fontColor See {@link #fontColor} */
+	/** @param fontColor Set the color used to draw text, this will override anything in this thing's buffer's {@link TextBuffer#options} */
 	public void setFontColor(ZColor fontColor){
-		this.fontColor = fontColor;
+		var options = this.getTextBuffer().getOptions();
+		var op = options.isEmpty() ? new TextOption("") : options.get(0);
+		
+		this.getTextBuffer().setOptions(ZArrayUtils.singleList(new TextOption(op.getText(), fontColor, op.getAlpha())));
 	}
 	
 	/** @return See {@link GameFont#size} */
@@ -218,7 +237,8 @@ public class MenuText extends MenuThing{
 		this.setTextY((this.getHeight() + height) * 0.5);
 	}
 	
-	/** Align the text so that the rightmost text pixel aligns to the right side of this thing
+	/**
+	 * Align the text so that the rightmost text pixel aligns to the right side of this thing
 	 *
 	 * @param offset The amount of pixels away from the right side
 	 */
@@ -244,7 +264,6 @@ public class MenuText extends MenuThing{
 		super.render(game, r, bounds);
 		
 		if(this.getFont() != null) r.setFont(this.getFont());
-		r.setColor(this.getFontColor());
 		r.setFontSize(this.getFontSize());
 		
 		if(this.getText() != null && !this.getText().isEmpty()) this.drawText(r, this.getText(), bounds);
